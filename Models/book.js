59705fd71f5b8027a7eb316c76c79b55b4d9ab34
Eladvain <eladvainberg@gmail.com>
2023-05-21 14:27:10 +0300

    add books file

---
+++ Models/book.js
@@ -0,0 +1,14 @@
+const { Module } = require("module");
+
+class Book
+{
+  constructor(name, id, author_id, img, price){
+    this.name = name;
+    this.id = id;
+    this.author = author_id;
+    this.image = img;
+    this.price = price;
+  }
+}
+
+module.exports = Book;
