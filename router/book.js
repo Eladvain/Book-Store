3ff72f6d2142cb97d9845a1492590886ae1bf2d0
Eladvain <eladvainberg@gmail.com>
2023-05-22 18:00:51 +0300

    update

--- router/book.js
+++ router/book.js
@@ -20,9 +20,32 @@ const set_content_type = function (req, res, next)
     msg:"All books returned"});	
 }
 
+async function list_all_books_by_author_id(req, res)
+{
+	const books = await utills.read_from_file(utills.constants_dict.books_file_name);
+	const id = parseInt(req.params.id);
+	if ( id <= 0)
+	{
+		res.status( StatusCodes.BAD_REQUEST );
+		res.send({msg:"Bad id given"}).status(StatusCodes.BAD_REQUEST)
+		return;
+	}
+	const books_by_author_id = books.filter(book => book.author === id );
+	
+	res.send({books:books_by_author_id,
+	          msg:"returned books by author id"} );   
+}
+
+async function create_new_book(req, res)
+{
+	
+}
+
 
 
 router.get('/books',  (req, res) => { list_all_books(req, res) });
+router.get('/books/(:id)',  (req, res) => { list_all_books_by_author_id(req, res) });
+router.post('/newBook',  (req, res) => { create_new_book(req, res) });
 
 router.use( set_content_type );
 module.exports = router;
